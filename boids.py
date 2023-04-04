7185d9a3b9a9c32ce398a41f7fed894efa6abe46
David Howe <david.howe@ontariotechu.net>
2023-04-04 18:23:23 -0400

    Update boids.py
    Adjusted default constants to make movement more accurate and added new constants to differentiate similar values

--- boids.py
+++ boids.py
@@ -1,7 +1,7 @@
 """
 === Pygame Flock Simulation ===
 authors: David Howe, Gerhard Yu
-last-edited: 2023/03/30 - 5:14pm
+last-edited: 2023/04/04 - 5:34pm
 """
 
 import math
@@ -21,17 +21,20 @@ BACKGROUND = BLACK
 
 # GLOBAL CONSTANTS
 ######################
-MIN_DISTANCE = 10
+MIN_DISTANCE = 20
+VISUAL_RANGE = 105
+BORDER_MARGIN = 70
+TURN_SPEED = 2
 
-COHERENCE = 100
-SEPARATION = 20
-ALIGNMENT = 5
+COHERENCE = 0.01
+SEPARATION = 0.05
+ALIGNMENT = 0.1
 
 SYS_HEIGHT  = 480
-SYS_WIDTH   = 640
+SYS_WIDTH   = 640 * 2
 
 OBJECTS = np.matrix([[50, 50]]) #object/obstacle can add more if needed
-SPEED_LIMIT = 0.1 #3/60 for m/s
+SPEED_LIMIT = 10
 ######################
 
 #unsure about this change if needed
@@ -49,8 +52,8 @@ class Boid(pygame.sprite.Sprite):
         self.x = random.uniform(-100, 100)
         self.y = random.uniform(-100, 100)
         # random speed
-        self.speed_x = 0
-        self.speed_y = 0
+        self.speed_x = random.uniform(-20, -10)
+        self.speed_y = random.uniform(-20, -10)
         
         # pygame
         pygame.sprite.Sprite.__init__(self)
@@ -75,24 +78,23 @@ class Boid(pygame.sprite.Sprite):
                     nearby_boid_count += 1
                     
         # Avoid divide by zero
-        if (nearby_boid_count == 0): 
-            nearby_boid_count = 1
+        if (nearby_boid_count != 0): 
         
-        c_x = overall_x / nearby_boid_count
-        c_y = overall_y / nearby_boid_count
-        
-        # Move towards center
-        self.speed_x += (c_x - self.x) / COHERENCE
-        self.speed_y += (c_y - self.y) / COHERENCE
+            c_x = overall_x / nearby_boid_count
+            c_y = overall_y / nearby_boid_count
+
+            # Move towards center
+            self.speed_x += (c_x - self.x) * COHERENCE
+            self.speed_y += (c_y - self.y) * COHERENCE
         
     # RULE 2
     def separation(self, boids):
         # Move away from nearby boids
         for boid in boids:
             if self != boid:
-                if self.is_close_to(boid):
-                    self.speed_x += (self.x - boid.x) / SEPARATION
-                    self.speed_y += (self.y - boid.y) / SEPARATION
+                if self.is_close_to(boid, MIN_DISTANCE):
+                    self.speed_x += (self.x - boid.x) * SEPARATION
+                    self.speed_y += (self.y - boid.y) * SEPARATION
         
     # RULE 3
     def alignment(self, boids):
@@ -116,30 +118,32 @@ class Boid(pygame.sprite.Sprite):
         c_y = velocity_y / nearby_boid_count
         
         # Align with group
-        boid.speed_x += (c_x - boid.speed_x) / ALIGNMENT
-        boid.speed_y += (c_y - boid.speed_y) / ALIGNMENT
+        boid.speed_x += (c_x - boid.speed_x) * ALIGNMENT
+        boid.speed_y += (c_y - boid.speed_y) * ALIGNMENT
         
     # RULE 4
     def boundaries(self):
-        # TODO - AVOID SCREEN BORDER
-        # You can use SYS_HEIGHT & SYS_WIDTH to do this
-        if abs(self.x - SYS_WIDTH) < MIN_DISTANCE:
-            self.speed_x = 0 - self.speed_x
-        if abs(self.y - SYS_HEIGHT) < MIN_DISTANCE:
-            self.speed_y = 0 - self.speed_y
-        #pass
+        # Avoid screen edge
+        
+        if self.x + SYS_WIDTH / 2 < BORDER_MARGIN: # left
+            self.speed_x += TURN_SPEED
+        if SYS_WIDTH / 2 - self.x < BORDER_MARGIN: # right
+            self.speed_x -= TURN_SPEED
+        if self.y + SYS_HEIGHT / 2 < BORDER_MARGIN: # top
+            self.speed_y += TURN_SPEED
+        if SYS_HEIGHT / 2 - self.y < BORDER_MARGIN: # bottom
+            self.speed_y -= TURN_SPEED
         
     # RULE 5
     def speedlimit(self):
-        # TODO - CAP BOID SPEEDS AT A GIVEN LIMIT
-        # You can make a new global constant called SPEED_LIMIT to do this
-        if self.speed_x > SPEED_LIMIT:
-            self.speed_x = self.speed_x * SPEED_LIMIT
-        if self.speed_y > SPEED_LIMIT:
-            self.speed_y = self.speed_y * SPEED_LIMIT
-        #still unsure about this one
-        #pass
+        # Do not go over speed limit
+        current_speed = (self.speed_x**2 + self.speed_y**2)**0.5
+        
+        if current_speed > SPEED_LIMIT:
+            self.speed_x = self.speed_x / current_speed * SPEED_LIMIT
+            self.speed_y = self.speed_y / current_speed * SPEED_LIMIT
 
+    # RULE 6
     def avoidObject(self):
         size = np.shape(OBJECTS)
         for columns in range(size[0]):
@@ -159,15 +163,15 @@ class Boid(pygame.sprite.Sprite):
         self.separation(boids)
         self.alignment(boids)
         self.boundaries()
-        self.avoidObject()
+#         self.avoidObject()
         self.speedlimit()
         
         # Move boid
         self.x += self.speed_x
         self.y += self.speed_y
 
-    def is_close_to(self, other):
-        return abs((self.x - other.x)) < MIN_DISTANCE and abs((self.y - other.y)) < MIN_DISTANCE
+    def is_close_to(self, other, distance = VISUAL_RANGE):
+        return abs((self.x - other.x)) < distance and abs((self.y - other.y)) < distance
     
 
 
@@ -221,7 +225,7 @@ class System:
 def main():
     # LOCAL CONSTANTS
     ########################
-    NUM_BOIDS   = 20
+    NUM_BOIDS   = 50
     BOID_RADIUS = 10
     ########################
     
@@ -248,8 +252,8 @@ def main():
 
     # Generate obstacles, remove if needed
     size = np.shape(OBJECTS)
-    for x in range(size[0]):
-        system.add_obstacle(OBJECTS[x])
+#     for x in range(size[0]):
+#         system.add_obstacle(OBJECTS[x])
     
     # Run simulation
     total_frames = 1000000
